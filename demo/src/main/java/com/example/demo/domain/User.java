6e3d4ab6fa5b5a6be90e49c6f12cdba2463bae5c
steve12512 <86954812+steve12512@users.noreply.github.com>
2026-02-03 22:37:19 +0200

    modifies User to extend the UserDetails class

--- demo/src/main/java/com/example/demo/domain/User.java
+++ demo/src/main/java/com/example/demo/domain/User.java
@@ -1,10 +1,16 @@
 package com.example.demo.domain;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table (name = "users")
-public class User {
+public class User implements UserDetails {
     String username;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,6 +20,36 @@ public class User {
     @Column(nullable = false)
     boolean isActive = true;
     String password;
+    String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isEnabled(){return  true;}
+
+    @Override
+    public boolean isCredentialsNonExpired(){return  true;}
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return  true;
+    }
+
+
 
     public String getUsername() {
         return username;
